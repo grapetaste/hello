412c732fc7a7bead678c0ab8dc4be5185dd6dec8
23kya23 <86909494+23kya23@users.noreply.github.com>
2021-07-28 16:20:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ def MBRSig():
     mbr = open("mbr.bin", 'rb')    #덤프 파일 열기
 
     data = mbr.read()   #읽은 파일 저장
-
-    print("[-] MBR Dump sha256: "+hashlib.sha256(data).hexdigest())
+    print("[-] MBR Dump MD5: "+hashlib.MD5(data).hexdigest())
     print("[-] MBR Dump SHA1: "+hashlib.sha1(data).hexdigest())
-#MD5는 보안 관련 위험성(?) 때문에 이젠 잘 안 쓴다고 하여 sha256으로 바꿔봤습니다
+    print("[-] MBR Dump sha256: "+hashlib.sha256(data).hexdigest())
+#MD5, SHA1은 취약점, 위험성 때문에 잘 안 쓴다고 하여 sha256을 추가해봤습니다
     mbr.close()
