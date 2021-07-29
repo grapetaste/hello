5cd4cf449790c3acd1b14e4bda1b5cdcdc019190
23kya23 <86909494+23kya23@users.noreply.github.com>
2021-07-29 17:03:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def MBRSig():
     print("MBR Dump SHA256:"+hashlib.sha256(data).hexdigest())
     #MD5, SHA1은 취약점, 위험성 때문에 잘 안 쓴다고 하여 sha256을 추가해봤습니다
 
-    sign1 = data[511:512].hex()
+    sign1 = data[511:512].hex() #.encode("hex") -> Attribute error -> .hex()로 변경
     sign2 = data[510:511].hex()
 
     print("\nMBR Safety Check:")
