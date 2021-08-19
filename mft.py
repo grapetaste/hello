0440f192ba307a4dff3046b43fad77b7201eebdc
23kya23 <86909494+23kya23@users.noreply.github.com>
2021-08-19 23:33:40 +0900

    Update mft.py

--- mft.py
+++ mft.py
@@ -1,25 +1,31 @@
-import os
-import sys
-
-f = open('test.001','rb+')
+f = open('\\\\.\\PhysicalDrive1','rb')
 f.seek(0) #커서를 맨처음으로 이동
 
-def HtoD(a , b):
-    f.seek(a)   #해당 위치 이동
-    str = f.read(b).hex()   #해당 크기 만큼 읽기
+def HtoD(a, b, c):  #Hex to Dec(해당 위치, 기준점, 해당 크기)
+    f.seek(a, b)   #해당 위치 이동
+    str = f.read(c).hex()   #해당 크기 만큼 읽기
     conv = ''.join([str[i-2:i] for i in range(len(str), 0, -2)])  #little-endian 변환
-    result = int(conv, 16)
+    result = int(conv, 16)  #10진수 변환
     return result
 
 st = 512    #sector
 
-f.seek(HtoD(0x1c6, 4)*st)    #VBR 섹터로 이동
+VBR_l = HtoD(0x1c6, 0, 4)
 
-S_o_M = HtoD(0x30, 8)   #Start Cluster for $MFT
-S_p_C = HtoD(0x13, 2)   #Sector per Cluster
-print(S_o_M, S_p_C)
+f.seek(VBR_l*st)    #VBR 섹터로 이동
+S_o_M = HtoD(0x30, 1, 8)   #Start Cluster for $MFT
 
-'''offset = 0
+f.seek(HtoD(0x1c6, 0, 4)*st)    #VBR 섹터로 이동
+S_p_C = HtoD(0x0D, 1, 2)   #Sector per Cluster
+
+MFT_l = S_o_M * S_p_C + VBR_l   #MBT 위치
+
+F_N = 48    #$File_Name
+f.seek((MFT_l + F_N) * st)    #$File_Name 위치로 이동
+print(f.tell()/512)
+S_o_f = HtoD(0x30, 1, 8)
+
+offset = 0
 f_size = getsize(f) #파일 사이즈 얻는 코드
 
 while f_size = offset:  #파일사이즈가 0이 될 때까지 반복
@@ -53,7 +59,3 @@ while data_size == 0:
             파일 생성 .xlsx로 저장
             for i in range(0, [파일크기])
             liness[i] = lines[i].split("숫자", % n번째 숫자)
-    
-
-
-* MFT > File record > $Filename(48(0x30)): 파일크기 알아내기'''
